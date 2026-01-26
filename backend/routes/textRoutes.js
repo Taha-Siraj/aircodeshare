f4bfd5d6e1db0aaadd44c2e53501e64a0dd6ed51
Taha-Siraj <pikachugaming899@gmail.com>
2026-01-26 23:11:57 +0500

    new

--- backend/routes/textRoutes.js
+++ backend/routes/textRoutes.js
@@ -3,7 +3,7 @@ import { createText, getText } from "../controllers/textcontrollers.js";
 
 const router = express.Router();
 
-router.post("/text", createText);
-router.get("/text", getText);
+router.post("/create", createText);
+router.get("/get", getText);
 
 export default router;
